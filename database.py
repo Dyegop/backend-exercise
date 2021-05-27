40ca2eaf5776a9021e5901005a9fb96a03d8f804
Diego <poncelas91@gmail.com>
2021-05-27 17:02:36 +0200

    Added connection to ElasticSearch

--- database.py
+++ database.py
@@ -1,12 +1,24 @@
 """ Connection to  Elastic Search database and methods to load a csv file into that database """
 
+import json
 from elasticsearch import Elasticsearch
 
 
 
+CREDENTIALS = "resources/credentials.json"
+
+
 def ElasticSearchConnection():
     """Connect to ElasticSearch"""
-    pass
+    try:
+        _USER, _PASSWORD, _CLOUD_ID = json.load(open(CREDENTIALS)).values()
+        return Elasticsearch(cloud_id=_CLOUD_ID, http_auth=(_USER, _PASSWORD), timeout=60)
+    except ConnectionError:
+        print("Error connecting to ElasticSearch\n")
+        raise
+    except FileNotFoundError:
+        print(f"File {CREDENTIALS} not found\n")
+        raise
 
 
 def loadCsv():
@@ -18,3 +30,10 @@ def getData():
     """ Query data from ElasticSearch """
     pass
 
+
+
+
+
+if __name__ == '__main__':
+    es = ElasticSearchConnection()
+    print(es.ping())
