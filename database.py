c599dc99a201a81eae280ca3f956aed85c36cc35
Diego <poncelas91@gmail.com>
2021-05-27 16:55:40 +0200

    Initial commit

---
+++ database.py
@@ -0,0 +1,20 @@
+""" Connection to  Elastic Search database and methods to load a csv file into that database """
+
+from elasticsearch import Elasticsearch
+
+
+
+def ElasticSearchConnection():
+    """Connect to ElasticSearch"""
+    pass
+
+
+def loadCsv():
+    """Load csv file into ElasticSearch"""
+    pass
+
+
+def getData():
+    """ Query data from ElasticSearch """
+    pass
+
